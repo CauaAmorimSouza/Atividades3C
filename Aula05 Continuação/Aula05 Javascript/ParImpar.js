ca9346f1cc8c9ea113d44d0716408ac765954108
Cauã Souza <caua_a_souza@estudante.sesisenai.org.br>
2024-02-26 08:12:30 -0300

    Create ParImpar.js

---
+++ Aula05 Continuação/Aula05 Javascript/ParImpar.js
@@ -0,0 +1,9 @@
+function ParImpar(p){
+    if(p % 2 === 0){
+        return console.log("O número escolhido é PAR")
+    }
+    else{
+       return console.log("O número escolhido é IMPAR")
+    }
+}
+ParImpar("3301")
