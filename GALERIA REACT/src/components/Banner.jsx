9a45cf5e2fcd3192f128b05ef64a09e58d8e7fa6
Cauã Souza <caua_a_souza@estudante.sesisenai.org.br>
2024-03-26 11:52:30 -0300

    Add files via upload

---
+++ GALERIA REACT/src/components/Banner.jsx
@@ -0,0 +1,7 @@
+export default function Banner(){
+    return(
+        <section className="Banner">
+            <img  src="./img/Ventilador.png"/>
+        </section>
+    )
+}
---
+++ GALERIA REACT/src/components/Sobre.jsx
@@ -0,0 +1,8 @@
+export default function Sobre(){
+    return(
+
+<section className="Sobre">
+<h1>Neste site você vai ver os melhores e mais atraentes eletrodomésticos que você precisa ter, AGORA!!!</h1>
+</section>
+    )
+}
