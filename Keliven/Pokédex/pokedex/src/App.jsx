d10f59ccde22e7d947e2b80445e99fe8f8ed47d7
Cauã Souza <caua_a_souza@estudante.sesisenai.org.br>
2024-06-04 08:54:56 -0300

    Pokédex

---
+++ Keliven/Pokédex/pokedex/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Pokedex from './components/Pokedex.jsx'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Pokedex />
+      <Footer />
+    </>
+  )
+}
+
+export default App
